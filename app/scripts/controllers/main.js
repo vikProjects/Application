8ae821ccc968b70c44e9eb82c321d3033eb402c1
unknown <Vikas Kahsyup-TCS@GGN000117127001.ads.mckinsey.com>
2014-06-26 12:28:40 +0530

    svg changes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -52,3 +52,33 @@ angular.module('mainappApp')
     };
 
   });
+
+
+
+
+  angular.module('mainappApp')
+  .controller('svgCtrl', function ($scope) {
+  var svg = d3.select('.svgClass')
+   .append("svg")
+   .attr("width", 500)
+   .attr("height", 500);
+
+   svg.append("circle").attr("cx", 55).attr("cy", 55).attr("r", 45).style("fill", "purple")
+   svg.append("ellipse").attr("cx",30).attr("cy",35).attr("rx",5).attr("ry",5).style("fill", "black")
+   svg.append("ellipse").attr("cx",75).attr("cy",35).attr("rx",5).attr("ry",5).style("fill", "black")
+   svg.append("ellipse").attr("cx",55).attr("cy",77).attr("rx",25).attr("ry",4).style("fill", "red")
+   svg.append("rect").attr("x",45).attr("y",73).attr("width",8).attr("height",8).style("fill", "white")
+   svg.append("rect").attr("x",55).attr("y",73).attr("width",8).attr("height",8).style("fill", "white")
+   svg.append("rect").attr("x",45).attr("y",99).attr("width",20).attr("height",30).style("fill", "white")
+   svg.append("circle").attr("cx", 55).attr("cy", 183).attr("r", 55).style("fill", "purple")
+   svg.append("rect").attr("x",25).attr("y",230).attr("width",20).attr("height",90).style("fill", "purple")
+   svg.append("rect").attr("x",65).attr("y",230).attr("width",20).attr("height",90).style("fill", "purple")
+  });
+
+
+
+
+
+
+
+
